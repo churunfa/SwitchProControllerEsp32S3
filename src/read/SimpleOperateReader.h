f51d94438835a735113c691200ee387533e75dbb
churunfa <47979382+churunfa@users.noreply.github.com>
2026-02-24 19:37:56 +0800

    基础指令封装

---
+++ src/read/SimpleOperateReader.h
@@ -0,0 +1,62 @@
+//
+// Created by churunfa on 2026/2/24.
+//
+
+#ifndef SWITCHPROCONTROLLERESP32S3_SIMPLEOPERATEREADER_H
+#define SWITCHPROCONTROLLERESP32S3_SIMPLEOPERATEREADER_H
+
+#include <ble/SwitchWakeUp.h>
+#include <debug/log.h>
+#include <debug/led_control.h>
+#include <ESP.h>
+
+#include "ReadStrategy.h"
+
+class SimpleOperateReader : public ReadStrategy {
+private:
+    uint8_t operationType = 0;
+    uint8_t readIndex = 0;
+    
+public:
+    void readData(const uint8_t inByte) override {
+        if (readIndex == 0) {
+            operationType = inByte;
+            readIndex++;
+        }
+    }
+
+    void exec() override {
+        logPrintf("SimpleOperateReader: 收到操作命令, type=%d\n", operationType);
+        
+        switch (operationType) {
+            case 0: // 唤醒操作
+                SwitchWakeUp::getInstance().trigger();
+                break;
+            case 1: // 重启操作
+                ESP.restart();
+                break;
+            case 2: // 连接手柄
+                GraphExecutor::getInstance().connectGamepad();
+                break;
+            case 3: // 拓扑图循环
+                GraphExecutor::getInstance().switchRunning(true);
+                break;
+            case 4: // 拓扑图停止
+                GraphExecutor::getInstance().switchRunning(false);
+                break;
+            default:
+                break;
+        }
+    }
+
+    void reset() override {
+        readIndex = 0;
+        operationType = 0;
+    }
+
+    int length() override {
+        return 1; // 需要读取1个字节作为操作类型
+    }
+};
+
+#endif //SWITCHPROCONTROLLERESP32S3_SIMPLEOPERATEREADER_H
